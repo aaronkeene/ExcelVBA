fd80d3b17e783adf6dd3dce1b3312089d425e4e9
aaronkeene <aaron@aaronkeene.com>
2016-05-30 10:03:06 -0500

    Create ReverseGeocode_SampleUsage.vb

---
+++ GoogleAPIs/ReverseGeocode_SampleUsage.vb
@@ -0,0 +1,27 @@
+Sub ReverseGeocode_Sample
+
+  Dim gc As GoogleReverseGeoCode
+  Dim key As String
+  
+  Set gc = New GoogleReverseGeoCode
+  key = "yourapikey"
+  
+  With gc
+    .Lattitude = 25
+    .Longitude = -45
+    .ApiKey = key
+        
+    MsgBox .ResponseProperty(Result)
+    MsgBox .ResponseProperty(Address_Type)
+    MsgBox .ResponseProperty(StreetAddress)
+    MsgBox .ResponseProperty(City)
+    MsgBox .ResponseProperty(SubDivision)
+    MsgBox .ResponseProperty(MainDivision)
+    MsgBox .ResponseProperty(PostalCode)
+    MsgBox .ResponseProperty(Country)
+      
+  End With
+      
+  Set gc = Nothing
+  
+End Sub
